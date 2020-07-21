79b228a7c2be9b7d62ec70a32ea65cfa2124227b
Danica Roth <droth@mines.edu>
2020-07-21 17:57:10 -0600

    Generalized code and added comments to allow more adaptation for other datasets.

--- Particle_Motion_exercise/make_filteredtopo.m
+++ Particle_Motion_exercise/make_filteredtopo.m
@@ -3,6 +3,14 @@
 % low-pass (long wavelength) rasters, roughness height distributions and
 % metadata.  
 % 
+% Note: If adjusting this code for different data, rasters (of surface-normal
+% distances from bare-ground plane) for each site should be contained in
+% geotiff files following naming format: L##.descriptors.tiff, where L is a
+% site identifier (B or V) and ## is a 2-digit slope angle measured in
+% degrees, and descriptors can be any additional naming details.
+% All *.rockdist.csv files must have 3 columns, with each column containing
+% data for the particle sizes described in the rockstats.mat data structure.
+% 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %  
 %
 % Written by Danica Roth, Colorado School of Mines, May 2019.
@@ -10,23 +18,28 @@
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
-clear
 
-% Identify site names
-topo.site={'B17' 'B20' 'B28' 'V14' 'V20' 'V24' 'V39'};
+clear
 
 % Set gaussian filter parameters
 filtsigma=100; %filter sigma value (in cm to match raster resolution)
 hsize=1.*filtsigma; %filter window size
 
-for i=1:length(topo.site)
+% Read in geotiff rasters and populate metadata fields in "topo" structure
+files=dir('Data/*.tif');
+topo.geotiff_file={files.name};
+for i=1:length(topo.geotiff_file)
+    eval(['topo.site{i}=''',topo.geotiff_file{i}(1:3),''';']);
     % Read in geotiff rasters and populate metadata fields in "topo" structure
-    topo.geotiff_file=[topo.site{i},'c-avgz-1cm-bgplaneN.tif'];
     topo.slopedeg(i)=str2num(topo.site{i}(2:3));
     topo.slope(i)=tan(deg2rad(topo.slopedeg(i)));
-    topo.raster{i}=imread(['Data/',topo.geotiff_file])';
+    topo.raster{i}=imread(['Data/',topo.geotiff_file{i}]);
+    [n,m]=size(topo.raster{i});
+    if n>m
+        topo.raster{i}=topo.raster{i}'; %reshape rasters so travel distance (assumed to be longer dimension) matches x dimension (columns)
+    end
     
-    % Generate high- and low-pass rasters
+    % Generate high- and low-pass rasters (short/long wavelength topography) 
     [topo.raster_lp{i},topo.raster_hp{i}] = nangaussfilt((topo.raster{i}),hsize,filtsigma,'plot','off'); 
     
     % Roughness calculations
--- Particle_Motion_exercise/make_rockdrops.m
+++ Particle_Motion_exercise/make_rockdrops.m
@@ -1,5 +1,12 @@
-% Generate structure containing empirical particle travel distance
+% Generate data structure containing empirical particle travel distance
 % distributions, raw experimental data and metadata. 
+% 
+% Note: If adjusting this code for different data, particle travel distance 
+% data for each site should be contained in csv files following naming format: 
+% L##.rockdist.csv, where L is a site identifier (B or V) and ## is a 2-digit 
+% slope angle measured in degrees. 
+% All *.rockdist.csv files must have 3 columns, with each column containing
+% data for the particle sizes described in the rockstats.mat data structure.
 % 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %  
 %
@@ -10,18 +17,14 @@
 
 clear
 
-% Read in data
+% Read in all data
 load('Data/rockstats.mat');
-B17 = csvread('Data/B17.rockdist.csv');
-B20 = csvread('Data/B20.rockdist.csv');
-B28 = csvread('Data/B28.rockdist.csv');
-V14 = csvread('Data/V14.rockdist.csv');
-V20 = csvread('Data/V20.rockdist.csv');
-V24 = csvread('Data/V24.rockdist.csv');
-V39 = csvread('Data/V39.rockdist.csv');
-
+files=dir('Data/*rockdist.csv');
+filenames={files.name};
+for i=1:length(filenames)
+    eval([filenames{i}(1:3),'=csvread(''Data/',filenames{i},''');']);
+end
 sites = who; %generate list of site names
-rocksize = {'small' 'med' 'large'};
 
 theta=0.1; %left truncation point (m)
 trunc=@(data) (data.x(data.x>theta)-theta); %anonymous function for left-truncating and re-zeroing data
@@ -30,10 +33,10 @@ CensorPoint = csvread('Data/censorpoints.csv',1,1)-theta; %right-censor points s
 for s=1:7
     for r=1:3 
         % Store raw experimental data and metadata in rockdrop.data structure
-        rockdrop{s,r}.data.SiteName = [sites{s},'_',rocksize{r}];
+        rockdrop{s,r}.data.SiteName = [sites{s},'_',rockstats(r).RockSize];
         rockdrop{s,r}.data.SlopeDeg=str2num(sites{s}(2:3));
         rockdrop{s,r}.data.Slope=tand(rockdrop{s,r}.data.SlopeDeg);
-        rockdrop{s,r}.data.RockSize=rocksize{r};
+%         rockdrop{s,r}.data.RockSize=rockstats(r).RockSize;
         rockdrop{s,r}.data.IntermediateAxis=rockstats(r).IntermediateAxis.mean;
         rockdrop{s,r}.data.Units='meters';
         rockdrop{s,r}.data.x=eval([sites{s},'(:,r)']); %Raw experimental travel distances
